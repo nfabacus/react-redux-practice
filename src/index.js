df8264570b1617b3dec6b2c45c45ce0abf06a182
nobuyuki.fujioka <nobuyuki.fujioka@ecotricity.co.uk>
2017-03-20 16:17:58 +0000

    adds functional components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = process.env.API_KEY;
+
 const App = () => {
-  return <div>Hello, world!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));

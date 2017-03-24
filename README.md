9b2f0d2f40608c7f8dc0a75a3f029ae714da6321
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-03-24 01:00:43 +0000

    extends app functional component to class based component

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 Practice for React and Redux
 
+If you want to run the app, you will need to create your .env file, with API_KEY=your-youtube-search-api-key.
 ```
 	> npm install
 	> npm start
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = process.env.API_KEY;
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboard'}, function(videos){
+      this.setState({ videos });
+      //Above is the same as: this.setState({ videos: videos });
+    });
+  }
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
+
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));

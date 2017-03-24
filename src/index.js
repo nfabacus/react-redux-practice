4f7e0926573c1188fe90ab740d766bee16ee973b
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-03-24 02:22:14 +0000

    adds video list item with key and props

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = process.env.API_KEY;
 
@@ -11,7 +12,7 @@ class App extends Component {
 
     this.state = { videos: [] };
 
-    YTSearch({ key: API_KEY, term: 'surfboard'}, function(videos){
+    YTSearch({ key: API_KEY, term: 'surfboard'}, (videos)=>{
       this.setState({ videos });
       //Above is the same as: this.setState({ videos: videos });
     });
@@ -20,6 +21,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos}/>
       </div>
     )
   }
